a763627c8e7e53c3fa6c88c1dd1a5353e150ddfe
Mateus Brigido <mateusbrigido@live.com>
2019-10-07 11:57:15 -0300

    Initial Commit

---
+++ Pokedex-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex-MVC
+//
+//  Created by Mateus Brigido on 07/10/19.
+//  Copyright © 2019 Mateus Brigido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
